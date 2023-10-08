0d65c0709b76261022d158544a28430a984820a3
Jairo Matos da Rocha <devjairomr@gmail.com>
2023-10-08 18:56:54 -0300

    limit

--- atlas/pasture_polygonize_simplified.py
+++ atlas/pasture_polygonize_simplified.py
@@ -352,7 +352,7 @@ def feature_loop(_docs):
         with MongoClient(MONGO) as client:
             db = client['polygonize']
             collection = db[BD_TABLE]
-            _docs = list(collection.find({'status': Status.PENDING.value})).limit(10000)
+            _docs = list(collection.find({'status': Status.PENDING.value}).limit(10000))
             pipeline = [
                 {
                     '$group': {
