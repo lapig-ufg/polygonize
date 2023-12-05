95a5c01c9cfcfd67a758fb9cbf5e1308361e6719
Jairo Matos da Rocha <devjairomr@gmail.com>
2023-12-05 19:11:49 -0300

    regions_v05_12_2023.shp

--- run_pasture.py
+++ run_pasture.py
@@ -2,12 +2,12 @@ from atlas.creat_quee import creat_feature_loop
 from atlas.pasture_polygonize import feature_loop
 
 args = (
-    '../Shapefile/regions_v19102023.shp',
+    '../Shapefile/regions_v05_12_2023.shp',
     '../Pastagem_Col8/pasture_br_Y*_COL8_atlas_sirgas.tif',
     '../Pastagem_Col8/pasture_br_Y',
     '_COL8_atlas_sirgas.tif',
 )
-datas = creat_feature_loop(args, 'pasture_col8')
+datas = creat_feature_loop(args, 'pasture_col8_s100')
 feature_loop(datas)
 
 
