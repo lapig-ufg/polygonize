81a501830777661af6176b5034a987e6a1063028
Jairo Matos da Rocha <devjairomr@gmail.com>
2023-12-21 09:46:21 -0300

    run

--- run_pasture_vigor.py
+++ run_pasture_vigor.py
@@ -1,8 +1,11 @@
-from atlas.pasture_vigor_polygonize import feature_loop
-from atlas.pasture_vigor_polygonize_simplified import \
-    feature_loop as feature_loop_s100
+from atlas.creat_quee import creat_feature_loop
+from atlas.pasture_vigor_polygonize_simplified import feature_loop
 
-# feature_loop_s100(('../Shapefile/regions_v26052023.shp','../Pastagem_Col8/pasture_br_Y*_COL8_atlas_sirgas.tif', '../Pastagem_Col8/pasture_br_Y','_COL8_atlas_sirgas.tif'))
-
-# feature_loop(('../Shapefile/regions_v26052023.shp','../Pastagem_Col8/pasture_br_Y*_COL8_atlas_sirgas.tif', '../Pastagem_Col8/pasture_br_Y','_COL8_atlas_sirgas.tif'))
-# feature_loop(('../Shapefile/amazonia.shp','../MariaHunter/amazonas_pastagem_integrado_*.tif', '../MariaHunter/amazonas_pastagem_integrado_','.tif'))
+args = (
+    '../Shapefile/regions_v05_12_2023.shp',
+    '../Pastagem_Col8/pasture_br_Y*_COL8_atlas_sirgas.tif',
+    '../Pastagem_Col8/pasture_br_Y',
+    '_COL8_atlas_sirgas.tif',
+)
+datas = creat_feature_loop(args, 'pasture_vigor_col8_S100')
+feature_loop(datas)
