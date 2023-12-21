d2bfb5700fecfdf6baef1b2a8babb84b4f357d39
Jairo Matos da Rocha <devjairomr@gmail.com>
2023-12-21 10:46:20 -0300

    pasture_cvp_veg_evi_mod13q1_2004_Brazil_col8_atlas_sirgas

--- run_pasture_vigor.py
+++ run_pasture_vigor.py
@@ -1,11 +1,13 @@
 from atlas.creat_quee import creat_feature_loop
 from atlas.pasture_vigor_polygonize_simplified import feature_loop
 
+
+#pasture_cvp_veg_evi_mod13q1_2004_Brazil_col8_atlas_sirgas
 args = (
     '../Shapefile/regions_v05_12_2023.shp',
-    '../Pastagem_Col8/pasture_br_Y*_COL8_atlas_sirgas.tif',
-    '../Pastagem_Col8/pasture_br_Y',
-    '_COL8_atlas_sirgas.tif',
+    '../Pastagem_Vigor_Col8/pasture_cvp_veg_evi_mod13q1_*_Brazil_col8_atlas_sirgas.tif',
+    '../Pastagem_Vigor_Col8/pasture_cvp_veg_evi_mod13q1_',
+    '_Brazil_col8_atlas_sirgas.tif',
 )
 datas = creat_feature_loop(args, 'pasture_vigor_col8_S100')
 feature_loop(datas)
